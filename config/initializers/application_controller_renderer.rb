15a0c7781deb762d3c172b5c42c02fbb9630c301
natsumi-m7 <n.osada0724@gmail.com>
2019-01-23 00:44:12 +0900

    [Finish]5-8

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'bcb5f13c508c27d3dd233e4c6580324f5ff56ff26f260dc1ae105217893fdb391dbf9fee055e3c652c090ed322866b12a7b891be1cf055246fc8f378727cfd3a'
